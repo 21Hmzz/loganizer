00eb91d619e23d41adc6646854b9bcc4090343a7
21Hmzz <hamza.bella2107@gmail.com>
2025-06-26 11:15:56 +0200

    squelette reporter

---
+++ internal/reporter/reporter.go
@@ -0,0 +1,9 @@
+package reporter
+
+//qd valentin aura push
+import "github.com/21Hmzz/loganalyzer/internal/analyzer"
+
+func Write(path string, results []analyzer.Result) error {
+    // todoo
+    return nil
+}
