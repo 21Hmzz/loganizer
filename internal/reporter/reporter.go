575fb8ccbd2a2191791daef5cb6cc4aaa2db9477
Pito <90179414+vpovedamaral@users.noreply.github.com>
2025-06-26 11:21:30 +0200

    Implémentation de Write

--- internal/reporter/reporter.go
+++ internal/reporter/reporter.go
@@ -1,8 +1,19 @@
 package reporter
 
-import "github.com/21Hmzz/loganalyzer/internal/analyzer"
+import (
+	"encoding/json"
+	"github.com/21Hmzz/loganalyzer/internal/analyzer"
+	"os"
+	"path/filepath"
+)
 
 func Write(path string, results []analyzer.Result) error {
-	// TODO
-	return nil
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return err
+	}
+	data, err := json.MarshalIndent(results, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
 }
